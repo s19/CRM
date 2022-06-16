7652eea88acad032e24745e4a5238dc705204c96
s19 <korsar_91sh@mail.ru>
2022-06-17 01:02:44 +0600

    Fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const h2 = document.querySelectorAll('modal__title');
-const modalForm = document.querySelectorAll('modal__form');
-const modalCheckbox = document.querySelectorAll('modal__checkbox');
-const modalCheckboxInput = document.querySelectorAll('modal__input_discount');
+const h2 = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('#discount');
+const modalCheckboxInput = document.querySelector('.modal__input_discount');
