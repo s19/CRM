ea80b7851cfeccf11f5469e7f51e3a7dc7a4a29c
s19 <korsar_91sh@mail.ru>
2022-06-17 00:53:05 +0600

    Add script

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const h2 = document.querySelectorAll('modal__title');
+const modalForm = document.querySelectorAll('modal__form');
+const modalCheckbox = document.querySelectorAll('modal__checkbox');
+const modalCheckboxInput = document.querySelectorAll('modal__input_discount');
